70aba74613004f82bf15eb49c01704905250187d
Masoud Ghiasi <masoud@ghiasi.me>
2018-10-14 02:02:08 +0330

    ref: Refactor packages
    related #MEN_3

---
+++ src/main/scala/backend/Snapshot.scala
@@ -0,0 +1,33 @@
+package backend
+
+import messages._
+
+object Snapshot {
+
+  final case class States(received: Map[String, String] = Map()) {
+    def updatedAdd(kv: Set): States = copy(received + (kv.key -> kv.value))
+
+    def updatedRemove(v: Remove): States = {
+      if (received.contains(v.key)) {
+        copy(received - v.key)
+      } else {
+        copy(received)
+      }
+    }
+
+    def getItem(k: GetItem): ResultMessage = {
+      if (received.contains(k.key)) {
+        SuccessJob(received(k.key))
+      } else {
+        FailedJob("invalid key")
+      }
+    }
+
+    def getAll: ResultMessage = {
+      SuccessJob(received.toString())
+    }
+
+    override def toString: String = received.toString
+  }
+
+}
